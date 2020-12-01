5bd0a248319be2c41ccd7c484c33068f62fe0f6b
umsyt <mxcheeks@gmail.com>
2020-12-01 13:57:32 +0000

    108

--- main.py
+++ main.py
@@ -17,16 +17,26 @@ with open('dict') as f:
     l = f.read().splitlines()
 
 bigboy = []
+yobgib = []
 
 for w in range(len(l)):
   bigboy.append(ordval(l[w]))
 
+for w in range(len(l)):
+  yobgib.append(droval(l[w]))
+
 dome = (staz.mode(bigboy))
 
 newguy = []
+fllla = []
 
 for w in range(len(l)):
-  if bigboy[w] == 113:
+  if (bigboy[w] == yobgib[w]):
     newguy.append(l[w])
+    fllla.append(bigboy[w])
 
-print(newguy)
+print(staz.mode(fllla))
+
+for w in range(len(newguy)):
+  if ordval(newguy[w]) == 108:
+    print(newguy[w])
