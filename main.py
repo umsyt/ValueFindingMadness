22654dc8cc690bd3de9affdeabf4e799035258b8
umsyt <mxcheeks@gmail.com>
2020-12-01 12:58:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import statistics as staz
+
+def ordval(s):
+  k = 0
+  for l in s:
+    k += ord(l)
+  k -= (len(s)*96)
+  return k
+
+def droval(s):
+  k = 0
+  for l in s:
+    k += (123-ord(l))
+  return k
+
+with open('dict') as f:
+    l = f.read().splitlines()
+
+bigboy = []
+
+for w in range(len(l)):
+  bigboy.append(ordval(l[w]))
+
+dome = (staz.mode(bigboy))
+
+newguy = []
+
+for w in range(len(l)):
+  if bigboy[w] == dome:
+    newguy.append(l[w])
+
+print(newguy)
